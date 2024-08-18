8a087456ef8b69d0550523eacf34f37cdd403729
abdellha <abdellahtaghouni@gmail.com>
2024-08-18 21:51:15 +0000

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+ef canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = boxes[0]
+
+    queue = keys
+
+    while queue:
+        key = queue.pop(0)
+        if key < n and not unlocked[key]:
+            unlocked[key] = True
+            queue.extend(boxes[key])
+
+    return all(unlocked)
