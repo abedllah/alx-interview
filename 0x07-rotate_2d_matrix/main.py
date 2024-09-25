a9014bf5f6c1c38a3b4898a2cd1d373c154da4da
abdellha <abdellahtaghouni@gmail.com>
2024-09-25 23:52:11 +0000

    fix pycode style

--- 0x07-rotate_2d_matrix/main.py
+++ 0x07-rotate_2d_matrix/main.py
@@ -5,7 +5,7 @@ Test 0x07 - Rotate 2D Matrix
 rotate_2d_matrix = __import__('0-rotate_2d_matrix').rotate_2d_matrix
 
 if __name__ == "__main__":
-    matrix = [[9,12, 3],
+    matrix = [[9, 12, 3],
               [4, 55, 6],
               [7, 8, 6]]
 
